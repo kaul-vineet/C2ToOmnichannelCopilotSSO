7fe84fa51877d4da6ce9a24a03e349345a11f0ea
Vineet Kaul <kaul.vineet@gmail.com>
2025-07-07 11:43:36 +0530

    unified.....omnichannel and copilot with dynamic token to Omnichannel enabled

--- routes/privatekey.js
+++ routes/privatekey.js
@@ -2,9 +2,12 @@ var jwt = require('jsonwebtoken');
 var fs = require('fs');
 var express = require('express');
 var router = express.Router();
+require("dotenv").config();
 
 /* GET home page. */
-router.get('/', function(req, res){
+router.get('/', async function(req, res){
+  
+  try {
     var privateKEY  = fs.readFileSync('./keys/private.key', 'utf8');
     const secondsSinceEpoch = Math.round(Date.now()/ 1000);
     const oneHour = 60*60;
@@ -13,25 +16,54 @@ router.get('/', function(req, res){
       algorithm: 	"RS256"
     };
 
-    var jwtPayload = {
+    const authHeaderToken = req.headers.authorization.split(' ')[1];
+    const authorizationServerId = process.env.OKTA_AUTHORIZATION_SERVER;
+    const yourOktaDomain = process.env.OKTA_ORG_URL;
+    const yourOktaClientID = process.env.OKTA_CLIENT_ID;
+
+  
+    // Get the payload from Okta based on the Okta token.
+    console.log("This is the url:", `${yourOktaDomain}/oauth2/${authorizationServerId}/v1/introspect?token=${authHeaderToken}&client_id=${yourOktaClientID}`);
+    
+    const resp = await fetch(
+      `${yourOktaDomain}/oauth2/${authorizationServerId}/v1/introspect?token=${authHeaderToken}&client_id=${yourOktaClientID}`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      }
+    );
+
+    const omnichannelResponseBody = await resp.json();
+    console.log("Response from introspection:", omnichannelResponseBody);
+
+    var jwtPayloadSample = {
         "sub": "66cb446f-5e43-ea11-a812-000d3a24c087", //contactid in Dynamics
-        "preferred_username": "kaul.vineet@microsoft.com",
+        "preferred_username": omnichannelResponseBody.username,
         "phone_number": "",
         "given_name": "Vineet",
         "family_name": "Kaul",
-        "email": "vineetkaul@microsoft.com",
+        "email": omnichannelResponseBody.username,
         "iat": secondsSinceEpoch,
         "exp": secondsSinceEpoch + oneHour,
-        "iss": "omnichannel-custom-portal.azurewebsites.net"
+        "iss": "copilot-d365-gqh5b8fwfnepdbdv.westus-01.azurewebsites.net"
     };
 
-    var token = jwt.sign(jwtPayload, privateKEY, signOptions);
+    console.log("This is the sampple payload to Omnichannel:", jwtPayloadSample);
+
+    var token = jwt.sign(JSON.stringify(jwtPayloadSample), privateKEY, signOptions);
+    // var token = jwt.sign(jwtPayload, privateKEY, signOptions);
+    console.log("This is the signed token:", token);
 
     res.charset = 'utf-8'
     res.set({
         'content-type': 'application/jwt'
     }).send(token);
-
+  } catch (error) {
+            // Handle error if needed
+            console.error("Error occurred while setting token:", error);
+        }
   });
 
 module.exports = router;
